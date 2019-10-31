7da85c71b57a287371022f80466f6c81390200d7
lustered <smellslikelavenders@gmail.com>
2019-10-31 18:41:49 -0400

    takes 1 arg as numeral

--- roman.py
+++ roman.py
@@ -1,12 +1,18 @@
 #!/usr/local/bin/python3
+import sys
 
-# takes a string ex: 'MDCIIIX'
+# takes an argument  ex: MDCIXI
+# otherwise uses a default roman numeral
+default_string = 'MDCXIII'
+if len(sys.argv) == 2 :
+    roman = str(sys.argv[1])
+else:
+    roman = default_string
+    print('Using default string: ' + default_string)
 
-roman = 'MDCIIIX'
 dic = {'M': 1000,'D': 500,'C':100,'L':50,'X':10,'V':5,'I':1}
 values = [value for value in roman]
 values = [dic[x] for x in values]
-#print(values)
 
 for n in range(len(values)-1):
     if n < n+1:
