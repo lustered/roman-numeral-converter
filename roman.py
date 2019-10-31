6228d9a46db36b81b3c310d2b51348e0a98e6707
lustered <smellslikelavenders@gmail.com>
2019-10-31 18:54:09 -0400

    fixing stupid mistake

--- roman.py
+++ roman.py
@@ -10,13 +10,16 @@ else:
     roman = default_string
     print('Using default string: ' + default_string)
 
+#key:value   numeral:value
 dic = {'M': 1000,'D': 500,'C':100,'L':50,'X':10,'V':5,'I':1}
 values = [value for value in roman]
 values = [dic[x] for x in values]
 
 for n in range(len(values)-1):
-    if n < n+1:
-        n *= -1
+    if values[n] < values[ n+1]:
+        values[n] *= -1
 
-print(sum(values))
+
+print('Value of: ' + roman +"\t"+ str(sum(values)))
+
 
