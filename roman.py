1102ffb5e9d684a3513cbdc70b9a65b685ee52eb
lustered <smellslikelavenders@gmail.com>
2019-11-04 09:43:18 -0500

    reverse support

--- roman.py
+++ roman.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3   
+from rome import Roman as rm
 import os
 import sys
 
@@ -6,32 +7,33 @@ import sys
 # otherwise uses a default roman numeral
 # can also take an int and convert it to a roman numeral 
 default_string = 'MDCXIII'
-arg = str(sys.argv[1]) if len(sys.argv) == 2 else default_string 
+arg = str(sys.argv[1]).upper() if len(sys.argv) == 2 else default_string 
 
-def get_values(arg, dic):
+def get_values(arg):
     values = [value for value in arg]
-    values = [dic[x] for x in values]
+    if not arg.isdigit():
+        dic = {'M': 1000,'D': 500,'C': 100,'L': 50,'X': 10,'V': 5,'I': 1}
+        values = [dic[x] for x in values]
 
-    for n in range(len(values)-1):
-        if values[n] < values[ n+1]:
-            values[n] *= -1
-
+        for n in range(len(values)-1):
+            if values[n] < values[ n+1]:
+                values[n] *= -1
+    else:
+        values = rm(int(arg))
     return values
 
 
 def converter(arg):
     print('Using string %s:' % (default_string if arg == default_string else arg))
-    dic = {'M': 1000,'D': 500,'C': 100,'L': 50,'X': 10,'V': 5,'I': 1}
     
-    if arg.isdigit():
-        inv_map = {value: key for key, value in dic.items()}
-        print(inv_map[int(arg)])
-    else:
-        values = get_values(arg, dic)
-        print(values)
-        print('Value of %s: \t %s' % (arg, str(sum(values))))
+    values = get_values(arg)
+    print('Value of %s: \t %s' % (arg, str(sum(values) if not arg.isdigit() else values)))
+
 
 if __name__ == '__main__':
-    converter(arg)
+    try:
+        converter(arg)
+    except KeyError:
+        print('Please enter a valid string or digit to convert')
 
 
